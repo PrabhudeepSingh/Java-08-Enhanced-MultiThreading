18c5baf32042c037c7b000b2ac5c31df8ac06ac3
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-16 13:53:49 +0530

    To illustrate the concept of Thread group in java.

---
+++ ThreadGroupDemo1.java
@@ -0,0 +1,8 @@
+class ThreadGroupDemo1
+{
+  public static void main(String[] args)
+  {
+    System.out.println(Thread.currentThread().getThreadGroup().getName());
+    System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
+  }
+}
